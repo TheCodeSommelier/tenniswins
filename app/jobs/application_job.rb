efba90e9867131ce0d544e99c4f402bde6a2d21a
Tony Masek <tm@tony-masek.com>
2024-09-13 06:12:47 +0400

    sdiekiq setup

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -2,8 +2,8 @@
 
 class ApplicationJob < ActiveJob::Base
   # Automatically retry jobs that encountered a deadlock
-  # retry_on ActiveRecord::Deadlocked
+  retry_on ActiveRecord::Deadlocked, attempts: 5, wait: :exponentially_longer
 
   # Most jobs are safe to ignore if the underlying records are no longer available
-  # discard_on ActiveJob::DeserializationError
+  discard_on ActiveJob::DeserializationError
 end
